49ce64ebc5c2aa1ca5ff7eeef24cea626b01e437
Olivier Van Goethem <ovg@google.com>
2020-02-26 18:16:56 +0000

    Script to set up infra + CI/CD script

--- infra/infra.sh
+++ infra/infra.sh
@@ -5,10 +5,13 @@ set -exu
 GCP_PROJECT="ovg-euro-2020"
 REGION="europe-west2"
 CLUSTER_NAME="euro-2020"
+DOMAIN="euro-2020.oliviervg.com"
 
 # Required env variables
 : "${OAUTH2_CLIENT_ID:?Variable not set or empty}"
 : "${OAUTH2_CLIENT_SECRET:?Variable not set or empty}"
+: "${FLASK_SECRET_KEY:?Variable not set or empty}"
+: "${FOOTBALL_API_KEY:?Variable not set or empty}"
 
 # Create static IP
 gcloud compute addresses create $CLUSTER_NAME --global \
@@ -30,16 +33,38 @@ gcloud beta container --project "${GCP_PROJECT}" clusters create "${CLUSTER_NAME
     --identity-namespace "${GCP_PROJECT}.svc.id.goog" \
     || echo "Cluster already exists!"
 
+# Wait for cluster to be fully available
+sleep 60
+
 # Get cluster credentials
 gcloud container clusters get-credentials $CLUSTER_NAME --region europe-west2 --project $GCP_PROJECT
 
+# Deploy sample app
+kubectl patch cm config-domainmapping -n knative-serving -p '{"data":{"autoTLS":"Enabled"}}'
+gcloud run deploy $CLUSTER_NAME \
+    --image gcr.io/cloudrun/hello:latest \
+    --memory=128Mi \
+    --platform gke \
+    --cluster $CLUSTER_NAME \
+    --cluster-location $REGION
+gcloud run domain-mappings create \
+    --service $CLUSTER_NAME \
+    --domain $DOMAIN \
+    --platform gke \
+    --cluster $CLUSTER_NAME \
+    --cluster-location $REGION
+
 # Apply modifications to cluster
-kubectl create secret generic $CLUSTER_NAME-oauth2 \
-    --from-literal=client_id=$OAUTH2_CLIENT_ID \
-    --from-literal=client_secret=$OAUTH2_CLIENT_SECRET \
-    || echo "Secret already existst!"
 kubectl apply -f istio-healthcheck.yaml
 kubectl -n gke-system patch svc istio-ingress \
     --type=json -p="$(cat istio-ingress-patch.json)" \
     --dry-run=true -o yaml | kubectl apply -f -
 kubectl apply -f ingress.yaml
+
+# Wait for HTTPS ingress to be fully available
+sleep 300
+
+# Enable IAP on HTTPS ingress
+gcloud compute backend-services update $(gcloud compute backend-services list | grep gke-system-istio-ingress | cut -d' ' -f1) \
+    --global \
+    --iap=enabled,oauth2-client-id=$OAUTH2_CLIENT_ID,oauth2-client-secret=$OAUTH2_CLIENT_SECRET
